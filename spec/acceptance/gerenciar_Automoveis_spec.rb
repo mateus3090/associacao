92bacb33c5012a2e442c946f8fe06098eac5cf9c
Mateus Rosa <mateus3090@gmail.com>
2012-02-29 11:23:08 -0300

    Criar alterar  Automovel

--- spec/acceptance/gerenciar_Automoveis_spec.rb
+++ spec/acceptance/gerenciar_Automoveis_spec.rb
@@ -29,7 +29,32 @@ feature 'gerenciar_automoveis' do
   end
 
 
+scenario 'alterar automovel' do #, :javascript => true do
 
+  cliente = FactoryGirl.create(:cliente,:nome => 'joao')
+  automovel = FactoryGirl.create(:automovel)
+  
+  visit edit_automovel_path(automovel)
+  
+
+    fill_in 'automovel_codigo', :with => '01'
+    fill_in 'automovel_modelo', :with => 'bonito'
+    fill_in 'automovel_marca' , :with => 'fiat'
+    fill_in 'automovel_tipo' , :with => 'esporte'
+    select 'joao', :on => 'cliente'
+
+  click_button 'Salvar'
+
+    page.should have_content 'Codigo: 01'
+    page.should have_content 'Modelo: bonito'
+    page.should have_content 'Marca: fiat'
+    page.should have_content 'Tipo: esporte'
+    page.should have_content 'Cliente: joao'
+end
+
+ 
+end
+					
 	
 
 
